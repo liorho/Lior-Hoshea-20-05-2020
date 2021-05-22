bf071db7e3fa0dd5889875e3072f6825564c82be
Lior Hoshea <hoshealior@gmail.com>
2021-05-23 02:03:25 +0300

    fixed https issue

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
 export const cities = axios.create({
-  baseURL: 'http://dataservice.accuweather.com/locations/v1/cities',
+  baseURL: 'https://dataservice.accuweather.com/locations/v1/cities',
 })
 
 export const currentConditions = axios.create({
-  baseURL: 'http://dataservice.accuweather.com/currentconditions/v1',
+  baseURL: 'https://dataservice.accuweather.com/currentconditions/v1',
 })
 
 export const fiveDaysForecast = axios.create({
-  baseURL: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day',
+  baseURL: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day',
 })
 
 export const getCityByLatLon = axios.create({
-  baseURL: 'http://dataservice.accuweather.com/locations/v1/cities/geoposition',
+  baseURL: 'https://dataservice.accuweather.com/locations/v1/cities/geoposition',
 })
