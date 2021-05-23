87cf4dfde88dfe20bcd515dbd860610d7c616f61
Lior Hoshea <hoshealior@gmail.com>
2021-05-23 15:35:20 +0300

    fixed error message

--- src/components/common/Error.js
+++ src/components/common/Error.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-function Error(props) {
-  const { message, api } = props;
-  return <p>Error: {message}. Please try again later.</p>;
-}
+const Error = ({message}) => <p>Error: {message}. Please try again later.</p>;
 
 export default Error;
